df13967e7d2f825dfa5941850587f0682bda592b
Samuel Oliveira <samuel.virtuoso@hotmail.com>
2021-03-11 13:46:11 -0300

    Extração sendo feita com sucessa criando filtro de dados

---
+++ app/controllers/textract_controller.rb
@@ -0,0 +1,50 @@
+require 'aws-sdk'
+require 'rack'
+require 'rack/request'
+require 'json'
+
+class TextractController < ApplicationController
+  def index
+
+    arquivo = params[:arquivo]
+
+    s3 = Aws::S3::Resource.new(
+      region: ENV['REGION'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    )
+
+    obj = s3.bucket(ENV['BUCKET_NAME']).object(arquivo.original_filename)
+    obj.upload_file(arquivo.tempfile)
+
+    client = Aws::Textract::Client.new({ #Faz uma conexão com a aws usando o Textract. Obs: só esta funcionando a região aplicada.
+      region: ENV['REGION'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'], #pedir a kei e criar variavel de ambiente
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] #pedir o acesso e criar variavel de ambiente
+    })
+  
+    resposta = client.start_document_analysis({ #inicia uma detecção assincrona e retorna um objeto job_id
+        document_location: { # required
+          s3_object: {
+            bucket: ENV['BUCKET_NAME'], #nome do bucket que será lido
+            name: arquivo.original_filename, #nome do arquivo que será lido
+          }
+        },
+        feature_types: ["TABLES"] #o tipo do arquivo que será extraido no caso de pdf 'TABLES'
+    })
+    
+    begin
+      @resposta_get = client.get_document_analysis({ #analisa o job_id e retorna os dados
+        job_id: resposta[:job_id], # required
+      })
+    end while(@resposta_get[:job_status] == "IN_PROGRESS") #Fica lendo os dados até ele retorna que terminou
+    puts "O resultado de textos foi: #{@resposta_get.blocks[1].text}"
+    render :result
+  end
+
+  def result
+    if @resposta_get != nil
+      redirect_to "/result_textract"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
-  post "/upload", to: "home#ativar"
+  post "result_textract", to: "textract#index"
+  get "result_textract", to: "textract#result"
 end
